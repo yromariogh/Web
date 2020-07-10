401e5de8175c505b92bdbab70707be8e39d63fcc
ch0ry <juanpablito2412@hotmail.com>
2020-07-10 10:57:50 -0500

    Intento de mejora (Aún no terminado)

--- Taller5/validation.js
+++ Taller5/validation.js
@@ -6,24 +6,60 @@ var ccusuario = document.getElementById('ccusuario')
 var ccpaswd = document.getElementById('ccpaswd')
 var email = document.getElementById('email')
 var telefono = document.getElementById('telefono')
+var username = document.getElementById('id_usr')
 
 function validarFormulario()
 {
-    if (nombre.value === "" || nombre.value.length>25) 
+    if ( ccpaswd.value === "" || ccpaswd.value.length < 15 || ccpaswd.value.length > 20)
     {
-        alert("Escriba un nombre válido")
-        return false;
+        setErrorFor(ccpaswd, "Bad password")
     }
 
-    if (apellido.value === "" || apellido.value.length>25) 
+    else{
+        if (!(/[A-Z]/g.test(ccpaswd.value))){
+            setErrorFor(ccpaswd, "NO caps")
+        }
+        if (!(/\/|#|%|&/g.test(ccpaswd.value))){
+            setErrorFor(ccpaswd, "No special symbols")
+        }
+    }
+
+    if (username.value === "" || username.value.length < 10 || username.value.length > 20)
     {
-        alert("Escriba un apellido válido")
-        return false;
+        setErrorFor(username, "Bad username")
+        
+    }
+
+    if (nombre.value === "" || nombre.value.length > 25) 
+    {
+        setErrorFor(nombre, "Bad name")
+        
+    }
+
+    if (apellido.value === "" || apellido.value.length > 25) 
+    {
+        setErrorFor(apellido, "Bad last name")
+        
     }
 
     if ( !(/\bcll|cra|av|anv|trans/i.test(direccion.value)))
     {
-        alert("La direccion no es válida")
-        return false;
+        setErrorFor(direccion, "Bad address")
+        
     }
+    return false;
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'input-field col s12 error';
+    small.innerText = message;
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
 }
