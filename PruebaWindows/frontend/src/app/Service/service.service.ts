318e4074458894bf9757ca067fed88f08bf94e1b
ch0ry <noobypgr@gmail.com>
2020-09-18 01:29:29 -0500

    FINALLY

--- PruebaWindows/frontend/src/app/Service/service.service.ts
+++ PruebaWindows/frontend/src/app/Service/service.service.ts
@@ -8,7 +8,7 @@ export class ServiceService {
   
   constructor(private http:HttpClient) { }
 
-  Url='http://localhost:8080/project/personas';
+  Url='http://localhost:8080/api/v1/personas';
 
   getPersonas(){
     return this.http.get<Persona[]>(this.Url);
